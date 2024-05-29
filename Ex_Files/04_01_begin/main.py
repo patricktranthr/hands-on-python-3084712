173081edc45ee03839b9d9be9ef725e6eedb1780
patricktranthr <patrick.trant.hr@gmail.com>
2024-05-29 16:13:32 +0000

    Finished course

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -4,3 +4,9 @@ response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
 last_twenty_years = response.json()[1][:20]
+
+for year in last_twenty_years:
+  if year["value"] is None:
+    year["value"] = 0
+  display_width = year["value"] // 10_000_000
+  print(f'{year["date"]}: {year["value"]}', '=' * display_width)
